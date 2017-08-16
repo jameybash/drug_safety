cce5a2804c1c22f6c6cdcbdc4b6bab2b127046b1
Jamie Hampton <jhampton@agrilyst.com>
2017-08-16 09:26:30 -0400

    added the rest of the cell setups

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -8,6 +8,28 @@ class Cell < ApplicationRecord
   
   def self.setup_cells
     Cell.setup_lsd_cells
+    Cell.setup_mushrooms_cells
+    Cell.setup_dmt_cells
+    Cell.setup_mescaline_cells
+    Cell.setup_dox_cells
+    Cell.setup_nbomes_cells
+    Cell.setup_2cx_cells
+    Cell.setup_2xtc_cells
+    Cell.setup_5meoxxt_cells
+    Cell.setup_cannabis_cells
+    Cell.setup_ketamine_cells
+    Cell.setup_mxe_cells
+    Cell.setup_dxm_cells
+    Cell.setup_nitrous_cells
+    Cell.setup_amphetamines_cells
+    Cell.setup_mdma_cells
+    Cell.setup_cocaine_cells
+    Cell.setup_caffeine_cells
+    Cell.setup_alcohol_cells
+    Cell.setup_ghbgbl_cells
+    Cell.setup_opioids_cells
+    Cell.setup_tramadol_cells
+    Cell.setup_benzo_cells
   end
   
   def self.setup_lsd_cells
@@ -243,6 +265,165 @@ class Cell < ApplicationRecord
     Cell.create!(drug_1: cannabis, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
     Cell.create!(drug_1: cannabis, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 1))
     Cell.create!(drug_1: cannabis, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_ketamine_cells
+    ketamine = Drug.find_by(name: 'Ketamine')
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'MXE'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'DXM'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Nitrous'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Amphetamines'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'MDMA'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: ketamine, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_MXE_cells
+    mxe = Drug.find_by(name: 'MXE')
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'DXM'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Nitrous'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Amphetamines'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'MDMA'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 5))
+    Cell.create!(drug_1: mxe, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 4))
+  end
+  
+  def self.setup_dxm_cells
+    dxm = Drug.find_by(name: 'DXM')
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Nitrous'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Amphetamines'), categorization: Categorization.find_by(scale: 5))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'MDMA'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 5))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: dxm, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 6))
+  end
+  
+  def self.setup_nitrous_cells
+    nitrous = Drug.find_by(name: 'Nitrous')
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Amphetamines'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'MDMA'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: nitrous, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_amphetamines_cells
+    amphetamines = Drug.find_by(name: 'Amphetamines')
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'MDMA'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: amphetamines, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_mdma_cells
+    mdma = Drug.find_by(name: 'MDMA')
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Cocaine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: mdma, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 3))
+  end
+  
+  def self.setup_cocaine_cells
+    cocaine = Drug.find_by(name: 'Cocaine')
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'Caffeine'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 5))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: cocaine, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_caffeine_cells
+    caffeine = Drug.find_by(name: 'Caffeine')
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'Alcohol'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 3))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 2))
+    Cell.create!(drug_1: caffeine, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_alcohol_cells
+    alcohol = Drug.find_by(name: 'Alcohol')
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'GHB/GBL'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 5))
+    Cell.create!(drug_1: alcohol, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 4))
+  end
+  
+  def self.setup_ghbgbl_cells
+    ghbgbl = Drug.find_by(name: 'GHB/GBL')
+    Cell.create!(drug_1: ghbgbl, drug_2: Drug.find_by(name: 'Opioids'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ghbgbl, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ghbgbl, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: ghbgbl, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: ghbgbl, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_opioids_cells
+    opioids = Drug.find_by(name: 'Opioids')
+    Cell.create!(drug_1: opioids, drug_2: Drug.find_by(name: 'Tramadol'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: opioids, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: opioids, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 4))
+    Cell.create!(drug_1: opioids, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+  end
+  
+  def self.setup_tramadol_cells
+    tramadol = Drug.find_by(name: 'Tramadol')
+    Cell.create!(drug_1: tramadol, drug_2: Drug.find_by(name: 'Benzodiazepines'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: tramadol, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 6))
+    Cell.create!(drug_1: tramadol, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 6))
+  end
+  
+  def self.setup_benzo_cells
+    benzo = Drug.find_by(name: 'Benzodiazepines')
+    Cell.create!(drug_1: benzo, drug_2: Drug.find_by(name: 'MAOIs'), categorization: Categorization.find_by(scale: 1))
+    Cell.create!(drug_1: benzo, drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 2))
+    
+    Cell.create!(drug_1: Drug.find_by(name: 'MAOIs'), drug_2: Drug.find_by(name: 'SSRIs'), categorization: Categorization.find_by(scale: 6))
+    
   end
   
 end
