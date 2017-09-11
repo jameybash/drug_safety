b85a270fb5a703fef34d2b62e6a904515f7fca0a
Jamie Hampton <jhampton@agrilyst.com>
2017-09-11 14:46:52 -0400

    error handling

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -2,8 +2,8 @@
 class CellsController < ApplicationController
 
   def index
-    @d1 = Drug.find(params[:drug_1_id])
-    @d2 = Drug.find(params[:drug_2_id])
+    @d1 = Drug.find(params[:drug_1_id]) if params[:drug_1_id]
+    @d2 = Drug.find(params[:drug_2_id]) if params[:drug_2_id]
     @cell = Cell.find(params[:cell_id]) if params[:cell_id]
   end
   
@@ -12,7 +12,7 @@ class CellsController < ApplicationController
     d2 = Drug.find(params[:drug_2_id])
     
     @cell = Cell.find_cell(d1, d2)
-    redirect_to cells_path(cell_id: @cell.id, drug_1_id: d1.id, drug_2_id: d2.id)
+    redirect_to cells_path(cell_id: @cell.try(:id), drug_1_id: d1.id, drug_2_id: d2.id)
   end
 
 end
